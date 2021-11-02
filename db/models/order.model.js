2824d032eb928decf55c7d4861941b45dc29b399
cristian <crisdarroca@hotmail.com>
2021-11-01 21:10:36 -0500

    muchos a muchos

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -27,6 +27,18 @@ const OrderSchema = {
     field: 'created_at',
     defaultValue: Sequelize.NOW,
   },
+  total: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      if (this.items.length > 0) {
+        return this.items.reduce(
+          (total, item) => total + item.price * item?.OrderProduct?.amount,
+          0
+        );
+      }
+      return 0;
+    },
+  },
 };
 
 class Order extends Model {
@@ -34,12 +46,12 @@ class Order extends Model {
     this.belongsTo(models.Customer, {
       as: 'customer',
     });
-    // this.belongsToMany(models.Product, {
-    //   as: 'items',
-    //   through: models.OrderProduct,
-    //   foreignKey: 'orderId',
-    //   otherKey: 'productId',
-    // });
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId',
+    });
   }
 
   static config(sequelize) {
